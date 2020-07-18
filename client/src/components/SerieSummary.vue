0c58b32730e6f5b82a68ad56fa94eeb0e3467457
Florent Beauchamp <florent.beauchamp@ateliernature.net>
2020-07-18 14:41:20 +0200

    feat: add thumbnail, desc and link to detail to a serie summary

--- client/src/components/SerieSummary.vue
+++ client/src/components/SerieSummary.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="serie-summary">
-    Summary of a serie in a list {{serie.id}}
+  <div class="container">
+    <img :src="thumbnail">
+    <div class="details">
+      <h3>{{serie.title}}</h3>
+      <p>
+        {{serie.description}}
+      </p>
+        <router-link :to="link">Details</router-link>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: row
+  }
+  .details{
+    flex:1
+  }
+</style>
+
 <script>
 export default {
-  props: ['serie']
+  props: ['serie'],
+  computed: {
+    link: function () {
+      return '/serie/' + this.serie.id
+    },
+    thumbnail: function () {
+      const { path, extension } = this.serie.thumbnail
+      return path + '/portrait_medium.' + extension
+    }
+  }
 }
 </script>
