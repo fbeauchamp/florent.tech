7dbce619cc104a3dd2fcb2a58b45ca38f1b29c68
Florent Beauchamp <florent.beauchamp@ateliernature.net>
2020-07-19 10:48:23 +0200

    refacto : put marvel cache in serice instead of controller

--- server/src/Controller/MarvelController.php
+++ server/src/Controller/MarvelController.php
@@ -5,8 +5,6 @@
  use Symfony\Component\Routing\Annotation\Route;
 
  use App\Service\Marvel;
- use App\Entity\Cache;
-use DateTime;
 
 class MarvelController extends AbstractController
  {
@@ -24,49 +22,17 @@ class MarvelController extends AbstractController
     */
   public function serie(string $id, Marvel $marvel)
   {
-     
-    $serie = $this->getDoctrine()
-      ->getRepository(Cache::class)
-      ->find('serie'.$id);
-    if($serie){
-      return $this->json(json_decode($serie->getData()));
-    }
     $serie = $marvel->fetchSerie($id);
-    
-    $entityManager = $this->getDoctrine()->getManager();
-    $cached = new Cache();
-    $cached->setId('serie'.$id);
-    $cached->setData($serie);
-    $cached->setCreatedAt(null);
-    
-    $entityManager->persist($cached);
-    $entityManager->flush();
-
     return $this->json(
       json_decode($serie)
     );
   }
 
+   /**
+    * @Route("/marvel/character/{id}", methods={"GET","HEAD"})
+    */
   public function character(string $id, Marvel $marvel){
-      
-    
-    $character = $this->getDoctrine()
-      ->getRepository(Cache::class)
-      ->find('character'.$id);
-    if($character){
-      return $this->json(json_decode($character->getData()));
-    }
     $character = $marvel->fetchCharacter($id);
-    
-    $entityManager = $this->getDoctrine()->getManager();
-    $cached = new Cache();
-    $cached->setId('serie'.$id);
-    $cached->setData($character);
-    $cached->setCreatedAt(null);
-    
-    $entityManager->persist($cached);
-    $entityManager->flush();
-
     return $this->json(
       json_decode($character)
     );
--- server/src/Service/Marvel.php
+++ server/src/Service/Marvel.php
@@ -4,20 +4,33 @@ namespace App\Service;
 
 use DateTime;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Cache;
 
 class Marvel
 {
 
   private $client;
+  private $entityManager;
   private $baseUrl ='https://gateway.marvel.com:443/v1/public/';
 
-  public function __construct(HttpClientInterface $client)
+  public function __construct(HttpClientInterface $client, EntityManagerInterface $entityManager)
   {
-      $this->client = $client;
+    $this->client = $client;
+    $this->entityManager = $entityManager;
   }
 
-  private function makeQuery($endpoint, $parameters=[])
+  private function makeQuery($endpoint, $cacheId=null, $parameters=[])
   {
+    if($cacheId){
+      $cachedContent = $this->entityManager
+      ->getRepository(Cache::class)
+      ->find($cacheId);
+      if($cachedContent){
+        return $cachedContent->getData();
+      }
+    }
     $publicKey = $_ENV['MARVEL_PUBLIC_KEY'];
     $privateKey = $_ENV['MARVEL_PRIVATE_KEY'];
     $ts = (new DateTime())->getTimeStamp();
@@ -32,6 +45,15 @@ class Marvel
     ]);
     
     $content = $response->getContent();
+    if($cacheId){
+      $cached = new Cache();
+      $cached->setId($cacheId);
+      $cached->setData($content);
+      $cached->setCreatedAt(null);
+      
+      $this->entityManager->persist($cached);
+      $this->entityManager->flush();
+    }
     return $content;
   }
 
@@ -41,11 +63,11 @@ class Marvel
   }
   public function fetchSerie($id)
   {
-    return $this->makeQuery('series/'.$id);
+    return $this->makeQuery('series/'.$id, 'series/'.$id);
   }
   
   public function fetchCharacter($id)
   {
-    return $this->makeQuery('character/'.$id);
+    return $this->makeQuery('character/'.$id,'character/'.$id);
   }
 }
